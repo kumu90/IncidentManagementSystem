20c5748499be108852725401f40c45986709f550
Surendra Mahatara <smahatara@qpay.com.np>
2024-06-10 16:46:45 +0545

    crated new controller

---
+++ IncidentManagementSystem/Controllers/InstitutionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IncidentManagementSystem.Controllers
+{
+    public class InstitutionController : Controller
+    {
+        // GET: Institution
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
