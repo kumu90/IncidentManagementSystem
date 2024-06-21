900be9fe95f986c517877886696e7b1097981dfd
ku mu <kumu404s@gmail.com>
2024-06-21 15:09:24 +0545

    user and index update

--- IncidentManagementSystem.Model/UserDto.cs
+++ IncidentManagementSystem.Model/UserDto.cs
@@ -11,21 +11,7 @@ namespace IncidentManagementSystem.Model
     public class UserDto
     {
         public string Id { get; set; }
-        public string InstId { get; set; }
-        public string InstitutionName { get; set; }
-        public string Country { get; set; }
-        public string State { get; set; }
-        public string Address { get; set; }
-        public string ZipCode { get; set; }
-        public string ContactPersonAdmin { get; set; }
-        public string ContactPersonTechnical { get; set; }
-        public string ContactNumber { get; set; }
-        public string Email { get; set; }
-        public string ImageUrl { get; set; }
-        public string CreatedBy { get; set; }
-        public string CreatedDate { get; set; }
-        public string UpdatedBy { get; set; }
-        public string Flag { get; set; }
+        
       
     }
 
